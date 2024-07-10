decce475e7c6bd6f009e0b3f3d686447e60de293
root <dekunattou@gmail.com>
2024-07-10 22:56:18 +0000

    add

--- multiple_run.sh
+++ multiple_run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-./do_notebook_by_cli.sh lmsys/exp/exp001.ipynb
+# ./do_notebook_by_cli.sh lmsys/exp/exp001.ipynb
+# ./do_notebook_by_cli.sh lmsys/exp/exp006.ipynb
+./do_notebook_by_cli.sh lmsys/exp/exp007.ipynb
+./do_notebook_by_cli.sh lmsys/exp/exp004.ipynb
