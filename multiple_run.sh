e7f699d2334f94d485879c976e40344922238d38
root <dekunattou@gmail.com>
2024-07-19 10:18:20 +0000

    add

--- multiple_run.sh
+++ multiple_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# ./do_notebook_by_cli.sh lmsys/exp/exp018.ipynb # 途中
-# ./do_notebook_by_cli.sh lmsys/exp/exp020.ipynb # 途中
-./do_notebook_by_cli.sh lmsys/exp/exp019.ipynb # 途中
+# ./do_notebook_by_cli.sh lmsys/exp/exp022.ipynb
+./do_notebook_by_cli.sh lmsys/exp/exp025.ipynb
+./do_notebook_by_cli.sh lmsys/exp/exp026.ipynb
