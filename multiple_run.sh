e7f2c17db002fc04d39cd8e6554da35e23e11415
sinchir0 <dekunattou@gmail.com>
2024-07-22 14:59:44 +0000

    add

--- multiple_run.sh
+++ multiple_run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # ./do_notebook_by_cli.sh lmsys/exp/exp041.ipynb # 27b
-# ./do_notebook_by_cli.sh lmsys/exp/exp042.ipynb
-./do_notebook_by_cli.sh lmsys/exp/exp045.ipynb
+./do_notebook_by_cli.sh lmsys/exp/exp049.ipynb
