515cd19170d5177babe1eed6462acffcf90883ec
sinchir0 <dekunattou@gmail.com>
2024-08-04 03:40:51 +0000

    add

--- multiple_run.sh
+++ multiple_run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # ./do_notebook_by_cli.sh lmsys/exp/exp083.ipynb
-./do_notebook_by_cli.sh lmsys/eda/2408022231_make_valid_exp083.ipynb
+# ./do_notebook_by_cli.sh lmsys/eda/2408022231_make_valid_exp083.ipynb
+# ./do_notebook_by_cli.sh lmsys/exp/exp084.ipynb
+./do_notebook_by_cli.sh lmsys/exp/exp087.ipynb
+
